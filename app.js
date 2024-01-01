c6fa8003d2932b0a4f1580f119ed38e8c9e9eaeb
gappy198 <mygappy21@gmail.com>
2024-01-01 16:19:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const express = require('express')
 const app = express()
 const router = require('./routes/myRouter')
 const path = require('path')
+const productRouter = express.Router()
 
 app.use(router)
-app.use(express.static(path.join(__dirname,'public')))
-app.get('/', (req, res) => {
-    res.send("<h1>Hello World | Studio</h1>")
+
+app.set("views", './src/views');
+app.set("view engine", "ejs");
+
+productRouter.route("/").get((req, res) => {
+    res.send("Hello World || Products")
 })
+
+productRouter.route("/1").get((req, res) => {
+    res.send("Hello World || Products 1")
+})
+
+app.use("/products", productRouter)
+
+app.get('/', (req, res) => {
+    res.render('index', { username: 'GapDev', customers: ["Golf", "Gap", "Get"] })
+})
+
+app.use(express.static(path.join(__dirname, '/public/')))
 
 app.listen(5000, () => {
     console.log("start server port", 5000);
