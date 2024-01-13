bf29944083b04b95822a990f90344a0e4a9dcb77
gappy198 <mygappy21@gmail.com>
2024-01-13 18:53:07 +0700

    Update productsRouter.js

--- src/route/productsRouter.js
+++ src/route/productsRouter.js
@@ -2,13 +2,13 @@ const express = require('express')
 const productsRouter = express.Router()
 const products = require('./../../data/products.json')
 
-productsRouter.route("/").get((req, res) => {
-    res.render("products", {
+productsRouter.route('/').get((req, res) => {
+    res.render('products', {
         products,
     })
 })
 
-productsRouter.route("/:id").get((req, res) => {
+productsRouter.route('/:id').get((req, res) => {
     const id = req.params.id
     res.render("product", {
         product: products[id],
